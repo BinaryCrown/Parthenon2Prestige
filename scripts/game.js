0da0f57ddef5975e244ce9cbbfdc7febb012def3
Jayde 3 <93054701+BinaryCrown@users.noreply.github.com>
2023-01-18 17:59:48 +0000

    Lot of new code: changed the font, added site logo and started implementing nation progress saving etc. via OOP

--- scripts/game.js
+++ scripts/game.js
@@ -55,7 +55,7 @@ let woodDisplay = document.getElementById("woodDisplay");
 // Increments time and gives resources
 function tick() {
 	if (isTicking) {
-		setTimeout(function(){tick()},25);
+		setTimeout(function(){tick()},50);
 	}
 	else {
 		isTicking = true;
@@ -75,12 +75,72 @@ for (let i = 0; i < 1000; i++) {
 	tick();
 }
 
+const g = {
+	// Government type stats here
+	/* NAME		HAPPINESS	INTELLIGENCE	SPIRITUALITY	FREEDOM	SAFETY	GDPGROWTH	DEATH*/
+	cenrep: 	[0.5,		0.5,			0.5,			0.5,	0.5,	0.5,		0.5],
+	libconfed:	[0.6,		0.5,			0.6,			0.4,	0.4,	0.5,		0.5],
+	feddem:		[0.5,		0.5,			0.5,			0.6,	0.4,	0.5,		0.5],
+	demo:		[0.4,		0.5,			0.5,			0.6,	0.5,	0.5,		0.5],
+	monarcy:	[0.5,		0.5,			0.6,			0.4,	0.5,	0.5,		0.5],
+	hippie:		[0.5,		0.5,			0.7,			0.5,	0.4,	0.4,		0.5],
+	capdict:	[0.5,		0.5,			0.5,			0.4,	0.5,	0.6,		0.5],
+	libpolice:	[0.4,		0.5,			0.4,			0.6,	0.6,	0.5,		0.5],
+	demrep:		[0.4,		0.5,			0.5,			0.7,	0.4,	0.5,		0.5],
+	capstate:	[0.5,		0.4,			0.5,			0.5,	0.5,	0.6,		0.5],
+	libstate:	[0.4,		0.4,			0.4,			0.7,	0.6,	0.5,		0.5],
+	conserv:	[0.6,		0.4,			0.5,			0.4,	0.6,	0.5,		0.5],
+	progressv:	[0.4,		0.6,			0.5,			0.6,	0.4,	0.5,		0.5],
+	anarch:		[0.5,		0.5,			0.5,			0.8,	0.4,	0.5,		0.3],
+	ucapstate:	[0.5,		0.5,			0.5,			0.3,	0.4,	0.8,		0.5],
+	corpdict:	[0.4,		0.7,			0.5,			0.4,	0.3,	0.7,		0.5],
+	fmcap:		[0.5,		0.4,			0.5,			0.3,	0.5,	0.8,		0.5],
+	bendict:	[0.3,		0.6,			0.5,			0.6,	0.5,	0.5,		0.5],
+	dict:		[0.5,		0.6,			0.5,			0.4,	0.5,	0.5,		0.5],
+	demsoc:		[0.3,		0.7,			0.5,			0.7,	0.4,	0.4,		0.5],
+	libpara:	[0.4,		0.8,			0.5,			0.7,	0.4,	0.3,		0.4],
+	authdemoc:	[0.4,		0.5,			0.5,			0.6,	0.6,	0.4,		0.5],
+	libsocdem:	[0.2,		0.9,			0.5,			0.8,	0.3,	0.3,		0.5],
+	leftutop:	[0.5,		0.7,			0.5,			0.6,	0.4,	0.4,		0.4],
+	authmob:	[0.4,		0.5,			0.5,			0.9,	0.5,	0.4,		0.3],
+	totdict:	[0.5,		0.7,			0.5,			0.3,	0.5,	0.5,		0.5],
+	comdict:	[0.5,		0.9,			0.5,			0.4,	0.4,	0.3,		0.5],
+	commie:		[0.4,		0.7,			0.5,			0.7,	0.4,	0.3,		0.5],
+	socdict:	[0.4,		1,				0.5,			0.4,	0.3,	0.4,		0.5],
+	autoc:		[0.5,		0.8,			0.5,			0.1,	0.4,	0.7,		0.5],
+	oligarch:	[0.4,		0.8,			0.5,			0.3,	0.4,	0.6,		0.5],
+	plutoc:		[0.5,		0.5,			0.5,			0.5,	0.5,	0.5,		0.5],
+	neoliberal:	[0.5,		0.2,			0.5,			0.6,	0.6,	0.7,		0.4],
+	theoc:		[0.6,		0.5,			0.5,			0.4,	0.7,	0.3,		0.5],
+	neoconserv:	[0.4,		0.3,			0.5,			0.7,	0.5,	0.7,		0.4],
+	terrorist:  [0.5,		0.5,			0.5,			0.8,	0.7,	0.2,		0.3],
+	colonial:	[0.3,		0.9,			0.5,			1,		0.4,	0.1,		0.3],
+	technoc:	[0.4,		0.6,			0.4,			0.4,	0.5,	0.7,		0.5]
+}
+
+function statsFromGovtype (govtype) {
+	return g[govtype];
+}
+
 class Nation {
-	constructor(govprop, popstats, flags, resources, economy) {
+	constructor(govprop, flags, resources, economy) {
 		// govprop = [name, flag, government type, taxes, budget]
-		// popstats = [happiness, intelligence, spirituality, freedom, safety]
+		// popstats = [happiness, intelligence, spirituality, freedom, safety, gdpgrowth, death]
 		// flags = array of numbers encoding technologies, issues, etc. 
 		// resources = [wood, stone, steel, food, wheat, matter, oil, energy, pp, qp, money]
 		// economy = [gdp, industries]
+		this.govprop = govprop;
+		this.popstats = statsFromGovtype(govprop[2]);
+		this.flags = flags;
+		this.resources = resources;
+		this.economy = economy;
 	}
+}
+
+// taxes = [lowerclass, middleclass, upperclass, colony, corporate]
+// corporate = [news, transport, clothes, food, pharma, oil, guns, tech, insurance, jewellery, gambling, drugs, brewing]
+// budget = [ps, education, healthcare, infra, military, blackbudget]
+
+function MakeNewNation(govprop) {
+	return new Nation([govprop[0], govprop[1], govprop[2], [0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0]])
 }
