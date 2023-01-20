163bb9f816d626ea65693213754dd471245b2ad4
Jayde 3 <93054701+BinaryCrown@users.noreply.github.com>
2023-01-20 12:49:36 +0000

    Removed some code

--- scripts/game.js
+++ scripts/game.js
@@ -141,22 +141,6 @@ function MakeNewNation(govprop) {
 
 // Cookies to save game progress
 
-function getCookie(cname) {
-	let name = cname + "=";
-	let decodedCookie = decodeURIComponent(document.cookie)
-	let ca = decodedCookie.split(';')
-	for(let i = 0; i <ca.length; i++) {
-	  let c = ca[i];
-	  while (c.charAt(0) == ' ') {
-		c = c.substring(1)
-	  }
-	  if (c.indexOf(name) == 0) {
-		return c.substring(name.length, c.length)
-	  }
-	}
-	return "";
-  }
-
 function ExportProgress(nation) {
 	localStorage.setItem("currentnation", JSON.stringify(nation))
 }
